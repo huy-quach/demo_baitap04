45427e3be52efa3498b9a46e0862ec5e94eb0183
Huy Quach <quach.huy2015@gmail.com>
2023-10-18 13:48:23 +0700

    giai bai tap 04

---
+++ for.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // Sử dụng vòng lặp for để hiển thị các số từ 1 đến 10
+    for (int i = 1; i <= 10; i++) {
+        printf("%d\n", i); // Hiển thị giá trị của i
+    }
+
+    return 0;
+}
